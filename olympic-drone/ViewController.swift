d571e24b031f686c4251d23c6487d29bb426c815
Taka Wang <jamie.w@inwinstack.com>
2022-03-18 23:04:07 +0800

    💪 add layers

--- olympic-drone/ViewController.swift
+++ olympic-drone/ViewController.swift
@@ -7,7 +7,6 @@
 
 import UIKit
 
-// MARK: - Point
 struct Point: Decodable {
     let x, y: Int
     let color: String
@@ -17,7 +16,7 @@ struct Point: Decodable {
 //
 // ref: https://stackoverflow.com/a/50042872
 // ref: https://praveenkommuri.medium.com/how-to-read-parse-local-json-file-in-swift-28f6cec747cf
-func loadPoints(forName name: String) -> [Point] {
+func loadPoints(fileName name: String) -> [Point] {
   do {
     guard let filePath = Bundle.main.path(forResource: name, ofType: "json") else { return [] }
     let fileUrl = URL(fileURLWithPath: filePath)
@@ -31,15 +30,80 @@ func loadPoints(forName name: String) -> [Point] {
   }
 }
 
+// setup CALayers with points
+func setupLayers(fileName name: String, to targetLayer: CALayer) -> [CAShapeLayer] {
+  let points = loadPoints(fileName: "ring") // count: 782
+  
+  // setup layers
+  let layerCount = points.count
+  var layers = [CAShapeLayer]()
+  layers.reserveCapacity(layerCount)
+
+  for idx in 1..<layerCount {
+    let layer = CAShapeLayer()
+    
+    // MARK: - ugly short version
+    //layer.path = UIBezierPath(ovalIn: CGRect(x: points[idx].x, y: points[idx].y, width: 1, height: 1)).cgPath
+    
+    // path setup
+    let path = UIBezierPath()
+    let center = CGPoint(x: points[idx].x, y: points[idx].y)
+    path.move(to: CGPoint(x: points[idx].x, y: points[idx].y))
+    path.addArc(withCenter: center, radius: 2, startAngle: 0, endAngle: 2 * CGFloat.pi, clockwise: true)
+
+    // set layer path
+    layer.path = path.cgPath
+    layer.strokeColor = UIColor(hex: points[idx].color+"ff")?.cgColor
+    layer.fillColor = UIColor(hex: points[idx].color+"ff")?.cgColor
+    
+    // add layer to parent layer
+    targetLayer.addSublayer(layer)
+    
+    // append to return array
+    layers.append(layer)
+  }
+  return layers
+}
+
 class ViewController: UIViewController {
 
   override func viewDidLoad() {
     super.viewDidLoad()
-    // Do any additional setup after loading the view.
-    var points = loadPoints(forName: "ring")
-    print(points[0])
+    
+    // load layers
+    var layers = setupLayers(fileName: "ring", to: self.view.layer)
+    print(layers[0])
   }
 
 
+}
+
+// convert a hex color to a UIColor
+// ref: https://www.hackingwithswift.com/example-code/uicolor/how-to-convert-a-hex-color-to-a-uicolor
+extension UIColor {
+    public convenience init?(hex: String) {
+        let r, g, b, a: CGFloat
+
+        if hex.hasPrefix("#") {
+            let start = hex.index(hex.startIndex, offsetBy: 1)
+            let hexColor = String(hex[start...])
+
+            if hexColor.count == 8 {
+                let scanner = Scanner(string: hexColor)
+                var hexNumber: UInt64 = 0
+
+                if scanner.scanHexInt64(&hexNumber) {
+                    r = CGFloat((hexNumber & 0xff000000) >> 24) / 255
+                    g = CGFloat((hexNumber & 0x00ff0000) >> 16) / 255
+                    b = CGFloat((hexNumber & 0x0000ff00) >> 8) / 255
+                    a = CGFloat(hexNumber & 0x000000ff) / 255
+
+                    self.init(red: r, green: g, blue: b, alpha: a)
+                    return
+                }
+            }
+        }
+        return nil
+    }
 }
 
