70c2c2ecef1d36a3d06092b7f98690c72492dc25
Taka Wang <jamie.w@inwinstack.com>
2022-03-18 22:11:48 +0800

    🚀 Load points from json file

--- olympic-drone/ViewController.swift
+++ olympic-drone/ViewController.swift
@@ -7,11 +7,37 @@
 
 import UIKit
 
+// MARK: - Point
+struct Point: Decodable {
+    let x, y: Int
+    let color: String
+}
+
+// load all points from json file.
+//
+// ref: https://stackoverflow.com/a/50042872
+// ref: https://praveenkommuri.medium.com/how-to-read-parse-local-json-file-in-swift-28f6cec747cf
+func loadPoints(forName name: String) -> [Point] {
+  do {
+    guard let filePath = Bundle.main.path(forResource: name, ofType: "json") else { return [] }
+    let fileUrl = URL(fileURLWithPath: filePath)
+    let data = try Data(contentsOf: fileUrl)
+    let points = try JSONDecoder().decode([Point].self, from: data)
+    return points
+    
+  } catch {
+    print("Something went wrong: \(error)")
+    return []
+  }
+}
+
 class ViewController: UIViewController {
 
   override func viewDidLoad() {
     super.viewDidLoad()
     // Do any additional setup after loading the view.
+    var points = loadPoints(forName: "ring")
+    print(points[0])
   }
 
 
